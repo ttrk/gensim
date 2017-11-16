4a0c25025bd2010f048f4c2c16a3b693739f177a
Kaya Tatar <tatark@mit.edu>
2017-11-16 00:37:11 -0500

    add simple Pythia card for prompt photon events and add a shell script to run the generation

---
+++ pythia82/photonJet/runPythiaGenerateAndWrite.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+progPaths=(
+"./pythiaGenerateAndWrite.exe"
+);
+
+progCards=(
+"./photonJet/promptPhoton.cmnd"
+);
+
+outputFiles=(
+"./photonJet/pythiaGenerateAndWrite_promptPhoton.root"
+);
+
+arrayIndices=${!outputFiles[*]}
+for i1 in $arrayIndices
+do
+    progPath=${progPaths[i1]}
+    progCard=${progCards[i1]}
+    outputFile=${outputFiles[i1]}
+    outputFileLOG="${outputFile/.root/.log}"
+    outDir=$(dirname "${outputFile}")
+    mkdir -p $outDir
+    $progPath $progCard $outputFile &> $outputFileLOG &
+    echo "$progPath $progCard $outputFile &> $outputFileLOG &"
+done
+
+

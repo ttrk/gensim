3fa0d4d2b220ccd2ddac45902a5736de407548f7
Kaya Tatar <tatark@mit.edu>
2017-11-16 00:57:25 -0500

    add shell script to run fastjet clustering on prompt photon events.

---
+++ pythia82/photon/runPythiaClusterJets.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+progPaths=(
+"./pythiaClusterJets.exe"
+);
+
+inputFiles=(
+"./photon/pythiaGenerateAndWrite_promptPhoton.root"
+);
+
+outputFiles=(
+"./photon/pythiaClusterJets_promptPhoton.root"
+);
+
+arrayIndices=${!outputFiles[*]}
+for i1 in $arrayIndices
+do
+    progPath=${progPaths[i1]}
+    inputFile=${inputFiles[i1]}
+    outputFile=${outputFiles[i1]}
+    outputFileLOG="${outputFile/.root/.log}"
+    outDir=$(dirname "${outputFile}")
+    mkdir -p $outDir
+    $progPath $inputFile $outputFile &> $outputFileLOG &
+    echo "$progPath $inputFile $outputFile &> $outputFileLOG &"
+done
+
+

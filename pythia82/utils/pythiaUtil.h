75cd9c8fef74489883e20e205977c3a058224cf2
Kaya Tatar <tatark@mit.edu>
2017-11-22 00:57:29 -0500

    add isParton()

--- pythia82/utils/pythiaUtil.h
+++ pythia82/utils/pythiaUtil.h
@@ -14,12 +14,18 @@
 #define PYTHIAUTIL_H_
 
 bool isGamma(Pythia8::Particle particle);
+bool isParton(Pythia8::Particle particle);
 bool isQuark(Pythia8::Particle particle);
 bool isGluon(Pythia8::Particle particle);
 
 bool isGamma(Pythia8::Particle particle)
 {
     return (particle.id() == 22);
+}
+
+bool isParton(Pythia8::Particle particle)
+{
+    return ((particle.idAbs() > 0 && particle.idAbs() < 9) || (particle.id() == 21));
 }
 
 bool isQuark(Pythia8::Particle particle)

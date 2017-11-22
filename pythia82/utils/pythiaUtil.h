19192b53714003483d71cca3f766e0c1f647a176
Kaya Tatar <tatark@mit.edu>
2017-11-21 22:50:30 -0500

    add utilities related to Pythia

---
+++ pythia82/utils/pythiaUtil.h
@@ -0,0 +1,36 @@
+/*
+ * utilities related to Pythia.
+ */
+
+#include "Pythia8/Pythia.h"
+#include "Pythia8/Event.h"
+#include "Pythia8/Basics.h"
+
+#include <string>
+#include <vector>
+#include <iostream>
+
+#ifndef PYTHIAUTIL_H_
+#define PYTHIAUTIL_H_
+
+bool isGamma(Pythia8::Particle particle);
+bool isQuark(Pythia8::Particle particle);
+bool isGluon(Pythia8::Particle particle);
+
+bool isGamma(Pythia8::Particle particle)
+{
+    return (particle.id() == 22);
+}
+
+bool isQuark(Pythia8::Particle particle)
+{
+    return (particle.id() > 0 && particle.id() < 9);
+}
+
+bool isGluon(Pythia8::Particle particle)
+{
+    return (particle.id() == 21);
+}
+
+#endif /* PYTHIAUTIL_H_ */
+

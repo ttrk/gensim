d0ac9e1796aaf41ecac06bb3ba1a755d24b64e78
Kaya Tatar <tatark@mit.edu>
2017-11-25 17:54:33 -0500

    add fillFinalEvent() : generic function to extract the particles that exist after the hadronization machinery

--- pythia82/utils/pythiaUtil.h
+++ pythia82/utils/pythiaUtil.h
@@ -19,6 +19,7 @@ bool isGluon(Pythia8::Particle particle);
 bool isGamma(Pythia8::Particle particle);
 bool isNeutrino(Pythia8::Particle particle);
 void fillPartonLevelEvent(Pythia8::Event& event, Pythia8::Event& partonLevelEvent);
+void fillFinalEvent(Pythia8::Event& event, Pythia8::Event& finalEvent);
 
 bool isParton(Pythia8::Particle particle)
 {
@@ -64,6 +65,23 @@ void fillPartonLevelEvent(Pythia8::Event& event, Pythia8::Event& partonLevelEven
             partonLevelEvent[iNew].statusPos();
             partonLevelEvent[iNew].mothers(i, i);
             partonLevelEvent[iNew].daughters(0, 0);
+        }
+}
+
+/*
+ * derived from main73.cc example
+ * generic function to extract the particles that exist after the hadronization machinery.
+ */
+void fillFinalEvent(Pythia8::Event& event, Pythia8::Event& finalEvent)
+{
+    finalEvent.reset();
+    int nEventSize = event.size();
+    for (int i = 0; i < nEventSize; ++i)
+
+        if (event[i].isFinal()) {
+            int iNew = finalEvent.append(event[i]);
+
+            finalEvent[iNew].mothers(i, i);
         }
 }
 

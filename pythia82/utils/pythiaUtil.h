30827f9bdc3c373bbfb605229363421e62d30b13
Kaya Tatar <tatark@mit.edu>
2017-11-25 22:31:28 -0500

    add function isAncestor()

--- pythia82/utils/pythiaUtil.h
+++ pythia82/utils/pythiaUtil.h
@@ -20,6 +20,7 @@ bool isGluon(Pythia8::Particle particle);
 bool isGamma(Pythia8::Particle particle);
 bool isNeutrino(Pythia8::Particle particle);
 bool isCharged(Pythia8::Particle particle, Pythia8::ParticleData& particleData);
+bool isAncestor(Pythia8::Event* evtPtr, int iParticle, int iAncestor);
 void fillPartonLevelEvent(Pythia8::Event& event, Pythia8::Event& partonLevelEvent);
 void fillFinalEvent(Pythia8::Event& event, Pythia8::Event& finalEvent);
 
@@ -51,6 +52,61 @@ bool isNeutrino(Pythia8::Particle particle)
 bool isCharged(Pythia8::Particle particle, Pythia8::ParticleData& particleData)
 {
     return (particleData.charge(particle.id()) != 0);
+}
+
+/*
+ * re-implementation of the following : bool Particle::isAncestor(int iAncestor);
+ * uses an external Event* evtPtr, the rest is same as the original function
+ *
+ * Check whether a given particle is an arbitrarily-steps-removed
+ * mother to another. For the parton -> hadron transition, only
+ * first-rank hadrons are associated with the respective end quark.
+ */
+bool isAncestor(Pythia8::Event* evtPtr, int iParticle, int iAncestor)
+{
+
+  // Begin loop to trace upwards from the daughter.
+  if (evtPtr == 0) return false;
+  int iUp = iParticle;
+  int sizeNow = (*evtPtr).size();
+  for ( ; ; ) {
+
+    // If positive match then done.
+    if (iUp == iAncestor) return true;
+
+    // If out of range then failed to find match.
+    if (iUp <= 0 || iUp > sizeNow) return false;
+
+    // If unique mother then keep on moving up the chain.
+    int mother1up = (*evtPtr)[iUp].mother1();
+    int mother2up = (*evtPtr)[iUp].mother2();
+    if (mother2up == mother1up || mother2up == 0) {iUp = mother1up; continue;}
+
+    // If many mothers, except hadronization, then fail tracing.
+    int statusUp = (*evtPtr)[iUp].statusAbs();
+    if (statusUp < 81 || statusUp > 86) return false;
+
+    // For hadronization step, fail if not first rank, else move up.
+    if (statusUp == 82) {
+      iUp = (iUp + 1 < sizeNow && (*evtPtr)[iUp + 1].mother1() == mother1up)
+          ? mother1up : mother2up; continue;
+    }
+    if (statusUp == 83) {
+      if ((*evtPtr)[iUp - 1].mother1() == mother1up) return false;
+      iUp = mother1up; continue;
+    }
+    if (statusUp == 84) {
+      if (iUp + 1 < sizeNow && (*evtPtr)[iUp + 1].mother1() == mother1up)
+        return false;
+      iUp = mother1up; continue;
+    }
+
+    // Fail for ministring -> one hadron and for junctions.
+    return false;
+
+  }
+  // End of loop. Should never reach beyond here.
+  return false;
 }
 
 /*

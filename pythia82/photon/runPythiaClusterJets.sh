ab85109d5f8ebc042bbce383125bd3bb1b3acdef
Kaya Tatar <tatark@mit.edu>
2017-11-25 19:33:29 -0500

    run jet clustering for different cone sizes and constituent types

--- pythia82/photon/runPythiaClusterJets.sh
+++ pythia82/photon/runPythiaClusterJets.sh
@@ -1,38 +1,81 @@
 #!/bin/bash
 
-progPaths=(
-"./pythiaClusterJets.exe"
-);
+progPath="./pythiaClusterJets.exe"
 
 inputFiles=(
+"./photon/pythiaGenerateAndWrite_promptPhoton.root"
+"./photon/pythiaGenerateAndWrite_promptPhoton.root"
+"./photon/pythiaGenerateAndWrite_promptPhoton.root"
+"./photon/pythiaGenerateAndWrite_promptPhoton.root"
+"./photon/pythiaGenerateAndWrite_promptPhoton.root"
+"./photon/pythiaGenerateAndWrite_promptPhoton.root"
+"./photon/pythiaGenerateAndWrite_promptPhoton.root"
+"./photon/pythiaGenerateAndWrite_promptPhoton.root"
 "./photon/pythiaGenerateAndWrite_promptPhoton.root"
 );
 
 outputFiles=(
 "./photon/pythiaClusterJets_promptPhoton.root"
+"./photon/pythiaClusterJets_promptPhoton.root"
+"./photon/pythiaClusterJets_promptPhoton.root"
+"./photon/pythiaClusterJets_promptPhoton.root"
+"./photon/pythiaClusterJets_promptPhoton.root"
+"./photon/pythiaClusterJets_promptPhoton.root"
+"./photon/pythiaClusterJets_promptPhoton.root"
+"./photon/pythiaClusterJets_promptPhoton.root"
+"./photon/pythiaClusterJets_promptPhoton.root"
 );
 
 jetRadii=(
 "3"
+"3"
+"3"
+"4"
+"4"
+"4"
+"5"
+"7"
+"8"
 );
 
 minJetPts=(
-"10"
+"5"
+"5"
+"5"
+"5"
+"5"
+"5"
+"5"
+"5"
+"5"
+);
+
+constituentTypes=(
+"0"
+"1"
+"2"
+"0"
+"1"
+"2"
+"0"
+"0"
+"0"
 );
 
 arrayIndices=${!outputFiles[*]}
 for i1 in $arrayIndices
 do
-    progPath=${progPaths[i1]}
     inputFile=${inputFiles[i1]}
     outputFile=${outputFiles[i1]}
     jetRadius=${jetRadii[i1]}
     minJetPt=${minJetPts[i1]}
+    constituentType=${constituentTypes[i1]}
     outputFileLOG="${outputFile/.root/.log}"
     outDir=$(dirname "${outputFile}")
     mkdir -p $outDir
-    $progPath $inputFile $outputFile $jetRadius $minJetPt &> $outputFileLOG &
-    echo "$progPath $inputFile $outputFile $jetRadius $minJetPt &> $outputFileLOG &"
+    $progPath $inputFile $outputFile $jetRadius $minJetPt $constituentType &> $outputFileLOG &
+    echo "$progPath $inputFile $outputFile $jetRadius $minJetPt $constituentType &> $outputFileLOG &"
+    wait
 done
 
 

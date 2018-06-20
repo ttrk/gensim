1e3a5d66f42422dd08dc759cbffad068e5242cd8
Kaya Tatar <tatark@mit.edu>
2018-06-20 16:50:41 -0400

    add object to filter Pythia events based on particle/event info remove function that just became obsolete

--- pythia82/utils/pythiaUtil.h
+++ pythia82/utils/pythiaUtil.h
@@ -7,12 +7,248 @@
 #include "Pythia8/ParticleData.h"
 #include "Pythia8/Basics.h"
 
+#include "../../utilities/systemUtil.h"
+
 #include <string>
 #include <vector>
 #include <iostream>
 
 #ifndef PYTHIAUTIL_H_
 #define PYTHIAUTIL_H_
+
+/*
+ * object to filter Pythia events based on particle/event info
+ */
+struct pythiaFilter {
+    pythiaFilter() {
+        processCode = -1;
+        idAbs = 999999;
+        id = -999999;
+        minPt = 0;
+        maxPt = -1;
+        minEta = -999999;
+        maxEta = 999999;
+        minY = -999999;
+        maxY = 999999;
+        minPhi = -999999;
+        maxPhi = 999999;
+        minP = 0;
+        maxP = -1;
+        statusAbs = 0;
+        status = 0;
+        motherIdAbs = {};
+        motherId = {};
+    }
+    void parseFilter(std::string pythiaFilterStr) {
+        std::vector<std::string> filterArgs = split(pythiaFilterStr, ";");
+        parseFilter(filterArgs);
+    }
+    void parseFilter(std::vector<std::string> filterArgs) {
+
+        for (std::vector<std::string>::iterator it = filterArgs.begin(); it != filterArgs.end(); ++it) {
+            std::string filterArg = trim((*it));
+            std::string filterArgVal = trim(filterArg.substr(filterArg.find("=")+1));
+
+            if (filterArg.find("processCode") == 0) {
+                processCode = std::atoi(filterArgVal.c_str());
+            }
+            else if (filterArg.find("idAbs") == 0) {
+                idAbs = std::atoi(filterArgVal.c_str());
+            }
+            else if (filterArg.find("id") == 0) {
+                id = std::atoi(filterArgVal.c_str());
+            }
+            else if (filterArg.find("minPt") == 0) {
+                minPt = std::atof(filterArgVal.c_str());
+            }
+            else if (filterArg.find("maxPt") == 0) {
+                maxPt = std::atof(filterArgVal.c_str());
+            }
+            else if (filterArg.find("minEta") == 0) {
+                minEta = std::atof(filterArgVal.c_str());
+            }
+            else if (filterArg.find("maxEta") == 0) {
+                maxEta = std::atof(filterArgVal.c_str());
+            }
+            else if (filterArg.find("minY") == 0) {
+                minY = std::atof(filterArgVal.c_str());
+            }
+            else if (filterArg.find("maxY") == 0) {
+                maxY = std::atof(filterArgVal.c_str());
+            }
+            else if (filterArg.find("minPhi") == 0) {
+                minPhi = std::atof(filterArgVal.c_str());
+            }
+            else if (filterArg.find("maxPhi") == 0) {
+                maxPhi = std::atof(filterArgVal.c_str());
+            }
+            else if (filterArg.find("minP") == 0) {
+                minP = std::atof(filterArgVal.c_str());
+            }
+            else if (filterArg.find("maxP") == 0) {
+                maxP = std::atof(filterArgVal.c_str());
+            }
+            else if (filterArg.find("statusAbs") == 0) {
+                statusAbs = std::atoi(filterArgVal.c_str());
+            }
+            else if (filterArg.find("status") == 0) {
+                status = std::atoi(filterArgVal.c_str());
+            }
+            else if (filterArg.find("motherIdAbs") == 0) {
+
+                // motherId is a list starting with "{" and ending with "}"
+                // Ex. motherId={1,2,3,21,22,-1,-2,-3}
+                size_t posStart = filterArgVal.find("{");     // a valid list starts with '{' and ends with '}'
+                size_t posEnd   = filterArgVal.rfind("}");     // a valid list starts with '{' and ends with '}'
+                std::string filterArgValTmp = filterArgVal.substr(posStart+1, posEnd-posStart-1);
+                std::vector<std::string> filterArgVals = split(filterArgValTmp, ",", false);
+                if (filterArgVals.size() == 0) filterArgVals = {filterArgValTmp};
+
+                for (std::vector<std::string>::iterator it = filterArgVals.begin(); it != filterArgVals.end(); ++it) {
+                    motherIdAbs.push_back(std::atoi((*it).c_str()));
+                }
+            }
+            else if (filterArg.find("motherId") == 0) {
+
+                // motherId is a list starting with "{" and ending with "}"
+                // Ex. motherId={1,2,3,21,22,-1,-2,-3}
+                size_t posStart = filterArgVal.find("{");     // a valid list starts with '{' and ends with '}'
+                size_t posEnd   = filterArgVal.rfind("}");     // a valid list starts with '{' and ends with '}'
+                std::string filterArgValTmp = filterArgVal.substr(posStart+1, posEnd-posStart-1);
+                std::vector<std::string> filterArgVals = split(filterArgValTmp, ",", false);
+                if (filterArgVals.size() == 0) filterArgVals = {filterArgValTmp};
+
+                for (std::vector<std::string>::iterator it = filterArgVals.begin(); it != filterArgVals.end(); ++it) {
+                    motherId.push_back(std::atoi((*it).c_str()));
+                }
+            }
+        }
+    }
+    std::string print() {
+
+        std::string result;
+
+        result.append(Form("processCode = %d\n", processCode));
+        result.append(Form("idAbs = %d\n", idAbs));
+        result.append(Form("id = %d\n", id));
+        result.append(Form("minPt = %.3f\n", minPt));
+        result.append(Form("maxPt = %.3f\n", maxPt));
+        result.append(Form("minEta = %.3f\n", minEta));
+        result.append(Form("maxEta = %.3f\n", maxEta));
+        result.append(Form("minY = %.3f\n", minY));
+        result.append(Form("maxY = %.3f\n", maxY));
+        result.append(Form("minPhi = %.3f\n", minPhi));
+        result.append(Form("maxPhi = %.3f\n", maxPhi));
+        result.append(Form("minP = %.3f\n", minP));
+        result.append(Form("maxP = %.3f\n", maxP));
+        result.append(Form("statusAbs = %d\n", statusAbs));
+        result.append(Form("status = %d\n", status));
+        result.append("motherIdAbs = {");
+        for (int i = 0; i < motherIdAbs.size(); ++i) {
+            if (i < motherIdAbs.size() - 1)
+                result.append(Form("%d, ", motherIdAbs[i]));
+            else
+                result.append(Form("%d", motherIdAbs[i]));
+        }
+        result.append("}\n");
+        result.append("motherId = {");
+        for (int i = 0; i < motherId.size(); ++i) {
+            if (i < motherId.size() - 1)
+                result.append(Form("%d, ", motherId[i]));
+            else
+                result.append(Form("%d", motherId[i]));
+        }
+        result.append("}");
+        // do not put a new line to the end of the last line
+        return result;
+    }
+
+    bool eventPassedFilter(Pythia8::Info& info) {
+        if (processCode != -1 && processCode != info.code()) return false;
+
+        return true;
+    }
+    bool particlePassedFilter(Pythia8::Event& event, int iPart) {
+
+        if (idAbs != 999999 && idAbs != event[iPart].idAbs()) return false;
+        if (id != -999999 && id != event[iPart].id()) return false;
+        if (minPt > 0 && minPt > event[iPart].pT()) return false;
+        if (maxPt > 0 && maxPt < event[iPart].pT()) return false;
+        if (minEta > event[iPart].eta()) return false;
+        if (maxEta < event[iPart].eta()) return false;
+        if (minY > event[iPart].y()) return false;
+        if (maxY < event[iPart].y()) return false;
+        if (minPhi > event[iPart].phi()) return false;
+        if (maxPhi < event[iPart].phi()) return false;
+        if (minP > 0 && minP > event[iPart].pAbs()) return false;
+        if (maxP > 0 && maxP < event[iPart].pAbs()) return false;
+        if (statusAbs != 0 && statusAbs != event[iPart].statusAbs()) return false;
+        if (status != 0 && status != event[iPart].status()) return false;
+
+        if (motherIdAbs.size() > 0) {
+            int iMom1 = event[iPart].mother1();
+            int iMom2 = event[iPart].mother2();
+
+            bool passedMotherIdAbs = false;
+            for (std::vector<int>::iterator it = motherIdAbs.begin(); it != motherIdAbs.end(); ++it) {
+                if ((*it) == event[iMom1].idAbs() || (*it) == event[iMom2].idAbs()) {
+                    passedMotherIdAbs = true;
+                    break;
+                }
+            }
+
+            if (!passedMotherIdAbs) return false;
+        }
+        if (motherId.size() > 0) {
+            int iMom1 = event[iPart].mother1();
+            int iMom2 = event[iPart].mother2();
+
+            bool passedMotherId = false;
+            for (std::vector<int>::iterator it = motherId.begin(); it != motherId.end(); ++it) {
+                if ((*it) == event[iMom1].id() || (*it) == event[iMom2].id()) {
+                    passedMotherId = true;
+                    break;
+                }
+            }
+
+            if (!passedMotherId) return false;
+        }
+
+        return true;
+    }
+    bool passedFilter(Pythia8::Event& event, Pythia8::Info& info) {
+
+        if (!eventPassedFilter(info)) return false;
+
+        return passedFilter(event);
+    }
+    bool passedFilter(Pythia8::Event& event) {
+        int nEventSize = event.size();
+        for (int i = 0; i < nEventSize; ++i) {
+            if (particlePassedFilter(event, i)) return true;
+        }
+
+        return false;
+    }
+
+    int processCode;
+    int idAbs;
+    int id;
+    double minPt;
+    double maxPt;
+    double minEta;
+    double maxEta;
+    double minY;
+    double maxY;
+    double minPhi;
+    double maxPhi;
+    double minP;
+    double maxP;
+    int statusAbs;
+    int status;
+    std::vector<int> motherIdAbs;
+    std::vector<int> motherId;
+};
 
 bool isParton(Pythia8::Particle particle);
 bool isQuark(Pythia8::Particle particle);
@@ -27,7 +263,6 @@ std::vector<int> daughterList(Pythia8::Event* evtPtr, int iPart);
 std::vector<int> daughterListRecursive(Pythia8::Event* evtPtr, int iPart);
 void fillPartonLevelEvent(Pythia8::Event& event, Pythia8::Event& partonLevelEvent);
 void fillFinalEvent(Pythia8::Event& event, Pythia8::Event& finalEvent);
-bool passedParticleFilter(Pythia8::Event& event, int idAbs, double ptMin = 0, bool isFinal = true, std::vector<int> motherIdAbs = {});
 
 bool isParton(Pythia8::Particle particle)
 {
@@ -242,33 +477,6 @@ void fillFinalEvent(Pythia8::Event& event, Pythia8::Event& finalEvent)
 
             finalEvent[iNew].mothers(i, i);
         }
-}
-
-bool passedParticleFilter(Pythia8::Event& event, int idAbs, double ptMin, bool isFinal, std::vector<int> motherIdAbs)
-{
-    int nEventSize = event.size();
-    for (int i = 0; i < nEventSize; ++i) {
-
-        if (isFinal && !event[i].isFinal()) continue;
-        if (idAbs != event[i].idAbs()) continue;
-        if (event[i].pT() < ptMin) continue;
-
-        int nMotherIdAbs = motherIdAbs.size();
-        bool passedMom = (nMotherIdAbs == 0);
-        for (int iMom = 0; iMom < nMotherIdAbs; ++iMom) {
-            if (motherIdAbs[iMom] != event[event[i].mother1()].idAbs())  continue;
-            if (motherIdAbs[iMom] != event[event[i].mother2()].idAbs())  continue;
-
-            event[i].motherList();
-
-            passedMom = true;
-            break;
-        }
-
-        if (passedMom) return true;
-    }
-
-    return false;
 }
 
 #endif /* PYTHIAUTIL_H_ */

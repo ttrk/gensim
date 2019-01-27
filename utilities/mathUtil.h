b409c0b7b7a61dc1207ab70d2d2cf300b9842450
Kaya Tatar <tatark@mit.edu>
2019-01-26 23:52:04 -0500

    math utilities, adopted from https://github.com/CmsHI/ElectroWeak-Jet-Track-Analyses/blob/master/Utilities/mathUtil.h

---
+++ utilities/mathUtil.h
@@ -0,0 +1,106 @@
+/*
+ * utilities related to math.
+ */
+
+#include <TString.h>
+#include <TMath.h>
+
+#include <string>
+#include <vector>
+#include <iostream>
+
+#ifndef MATHUTIL_H_
+#define MATHUTIL_H_
+
+typedef double (*fncPointer)(double *, double *);
+
+fncPointer getFncPointer(std::string fncName);
+int getFncNpar(std::string fncName);
+int getFncNpar(fncPointer fncPtr);
+double fnc_DSCB(double* xx, double* params);
+
+namespace MATHUTIL {
+
+enum FNCS {
+    k_DSCB,
+    kN_FNCS
+};
+const std::string FNCNAMES[kN_FNCS] = {
+        "fnc_DSCB"
+    };
+
+fncPointer FNCPOINTERS[kN_FNCS] = {
+        fnc_DSCB
+    };
+
+/*
+ * number of parameters for a function
+ */
+int FNCNPARS[kN_FNCS] = {
+        7
+    };
+}
+
+fncPointer getFncPointer(std::string fncName)
+{
+    for (int i = 0; i < MATHUTIL::kN_FNCS; ++i) {
+        if (fncName == MATHUTIL::FNCNAMES[i]){
+            return MATHUTIL::FNCPOINTERS[i];
+        }
+    }
+
+    return 0;
+}
+
+int getFncNpar(std::string fncName)
+{
+    return getFncNpar(getFncPointer(fncName));
+}
+
+/*
+ * returns the number of parameters for the given function
+ */
+int getFncNpar(fncPointer fncPtr)
+{
+    for (int i = 0; i < MATHUTIL::kN_FNCS; ++i) {
+        if (fncPtr == MATHUTIL::FNCPOINTERS[i]){
+            return MATHUTIL::FNCNPARS[i];
+        }
+    }
+
+    return 0;
+}
+
+/*
+ * Double sided crystal ball function
+ * The default crystal ball function has its tail to the left of Gaussian core.
+ * For details of the crystal function see : https://en.wikipedia.org/wiki/Crystal_Ball_function
+ */
+double fnc_DSCB(double* xx, double* params)
+{
+    double x   = xx[0];
+    // gaussian core
+    double N     = params[0];
+    double mean  = params[1];
+    double sigma = params[2];
+    // transition parameters
+    double a1  = params[3];
+    double n1  = params[4];
+    double a2  = params[5];
+    double n2  = params[6];
+
+    double u   = (x-mean)/sigma;
+    double A1  = TMath::Power(n1/TMath::Abs(a1),n1)*TMath::Exp(-a1*a1/2);
+    double A2  = TMath::Power(n2/TMath::Abs(a2),n2)*TMath::Exp(-a2*a2/2);
+    double B1  = n1/TMath::Abs(a1) - TMath::Abs(a1);
+    double B2  = n2/TMath::Abs(a2) - TMath::Abs(a2);
+
+    double res = N;
+    if      (u < -a1) res *= A1*TMath::Power(B1-u,-n1);
+    else if (u < a2)  res *= TMath::Exp(-u*u/2);
+    else              res *= A2*TMath::Power(B2+u,-n2);
+    return res;
+}
+
+#endif /* MATHUTIL_H_ */
+

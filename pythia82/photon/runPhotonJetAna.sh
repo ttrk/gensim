be5be3f70922b7c02b244d152d723f33cd5920c6
Kaya Tatar <tatark@mit.edu>
2019-02-11 17:10:59 -0500

    script to run photon+jet analysis

---
+++ pythia82/photon/runPhotonJetAna.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+progPath="./photon/photonJetAna.exe"
+
+eventFileBase="./out/events/photon/pythiaGenerateAndWrite_promptPhoton.root"
+eventFiles=(
+$eventFileBase
+$eventFileBase
+$eventFileBase
+$eventFileBase
+$eventFileBase
+$eventFileBase
+$eventFileBase
+$eventFileBase
+$eventFileBase
+$eventFileBase
+$eventFileBase
+$eventFileBase
+$eventFileBase
+);
+
+jetFileBase="./out/jets/photon/pythiaClusterJets_promptPhoton.root"
+jetFiles=(
+$jetFileBase
+$jetFileBase
+$jetFileBase
+$jetFileBase
+$jetFileBase
+$jetFileBase
+$jetFileBase
+$jetFileBase
+$jetFileBase
+$jetFileBase
+$jetFileBase
+$jetFileBase
+$jetFileBase
+);
+
+jetTrees=(
+"ak3jets"
+"ak3jetsCh"
+"ak3jetsParton"
+"ak3jetsPartonHard"
+"ak4jets"
+"ak4jetsCh"
+"ak4jetsParton"
+"ak4jetsPartonHard"
+"ak5jets"
+"ak7jets"
+"ak8jets"
+"ak8jetsParton"
+"ak8jetsPartonHard"
+);
+
+outputFileBase="./out/analysis/photon/photonJetAna_promptPhoton"
+outputFiles=(
+$outputFileBase"_ak3jets.root"
+$outputFileBase"_ak3jetsCh.root"
+$outputFileBase"_ak3jetsParton.root"
+$outputFileBase"_ak3jetsPartonHard.root"
+$outputFileBase"_ak4jets.root"
+$outputFileBase"_ak4jetsCh.root"
+$outputFileBase"_ak4jetsParton.root"
+$outputFileBase"_ak4jetsPartonHard.root"
+$outputFileBase"_ak5jets.root"
+$outputFileBase"_ak7jets.root"
+$outputFileBase"_ak8jets.root"
+$outputFileBase"_ak8jetsParton.root"
+$outputFileBase"_ak8jetsPartonHard.root"
+);
+
+photonType=1
+
+arrayIndices=${!outputFiles[*]}
+for i1 in $arrayIndices
+do
+    eventFile=${eventFiles[i1]}
+    jetFile=${jetFiles[i1]}
+    jetTree=${jetTrees[i1]}
+    outputFile=${outputFiles[i1]}
+    outputFileLOG="${outputFile/.root/.log}"
+    outDir=$(dirname "${outputFile}")
+    mkdir -p $outDir
+    $progPath $eventFile $jetFile $jetTree $outputFile $photonType &> $outputFileLOG &
+    echo "$progPath $eventFile $jetFile $jetTree $outputFile $photonType &> $outputFileLOG &"
+    wait
+done
+
+

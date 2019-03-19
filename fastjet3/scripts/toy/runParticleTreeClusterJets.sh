efcb6cbd5b3508bb8e9df8301939ce59d651f55b
Kaya Tatar <tatark@mit.edu>
2019-03-18 23:51:28 -0400

    remove unnecessary

--- fastjet3/scripts/toy/runParticleTreeClusterJets.sh
+++ fastjet3/scripts/toy/runParticleTreeClusterJets.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-runCmd=""
-if [ ! -z ${MYRUN} ]; then
-  runCmd=${MYRUN}
-fi
-
 fileSuffix="sampleToyEvents_cent010_N5000"
 inputFile="../toy/"$fileSuffix".root"
 outputFile="../toy/particleTreeClusterJets_"$fileSuffix".root"
